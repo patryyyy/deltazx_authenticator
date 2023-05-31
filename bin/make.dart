988287864e7e0b94c69388177a16987f7585547a
Patryyyy <78462427+patryyyy@users.noreply.github.com>
2023-05-31 16:10:12 +0800

    Change the build process and add release notes (#5)

--- bin/make.dart
+++ bin/make.dart
@@ -1,32 +1,122 @@
 import 'dart:io';
 
+import '../lib/src/info/version.dart';
+
 void main() {
-  if (Platform.isWindows) {
-    if (build.isX64()) {
-      build('1.0.0', 'windows', 'amd64', '.exe');
-    }
+  switch (Platform.operatingSystem) {
+    case "windows":
+      if (Windows.isX64) {
+        Builder(Windows()).buildApp(version, "x64");
+      }
+      else if (Windows.isARM64) {
+        Builder(Windows()).buildApp(version, "arm64");
+      }
+      break;
+    case "linux":
+      if (Linux.isX64) {
+        Builder(Linux()).buildApp(version, "x64");
+      }
+      else if (Linux.isARM64) {
+        Builder(Linux()).buildApp(version, "arm64");
+      }
+      else if (Linux.isRISCV) {
+        Builder(Linux()).buildApp(version, "riscv64");
+      }
+      break;
+    case "macos":
+      if (MacOS.isX64) {
+        Builder(MacOS()).buildApp(version, "x64");
+      }
+      else if (MacOS.isARM64) {
+        Builder(MacOS()).buildApp(version, "arm64");
+      }
+      break;
+    case "android":
+      if (Android.isARM64) {
+        Builder(Android()).buildApp(version, "arm64");
+      }
+      break;
   }
-  else if (Platform.isLinux) {
-    if (build.isX64()) {
-      build('1.0.0', 'linux', 'amd64', '');
-    }
-  }
-  else if (Platform.isMacOS) {
-    if (build.isX64()) {
-      build('1.0.0', 'darwin', 'amd64', '');
-    }
+
+}
+
+class Builder {
+  final BuildStrategy buildStrategy;
+
+  Builder(this.buildStrategy);
+
+  void buildApp(String version, String arch) {
+    print(Platform.version);
+    print(Process.runSync('dart', ['pub', 'get']).stdout.toString());
+    this.buildStrategy.build(version, arch);
   }
 }
 
-class build {
-  build(String version, String platform, String architecture, String fileExtension) {
-    Directory('./build').createSync();
-    Process.runSync('dart', ['pub', 'get']);
-    Process.runSync('dart', ['compile','exe','bin/deltazx_authenticator.dart','-o','build/dau-$version-$platform-$architecture'+'$fileExtension']);
+abstract class BuildStrategy {
+  void build(String version, String arch);
+}
+
+class Windows implements BuildStrategy {
+  static bool get isX64 {
+    return Platform.version.contains("x64");
   }
 
-  static bool isX64() {
-    print(Process.runSync('dart', ['--version']).stdout.toString());
-    return Process.runSync('dart', ['--version']).stdout.toString().contains('x64');
+  static bool get isARM64 {
+    return Platform.version.contains("arm64");
+  }
+
+  @override
+  void build(String version, String arch) {
+    Directory('build').createSync(recursive: true);
+    Process.runSync('dart', ['compile','exe','bin/deltazx_authenticator.dart','-o','build/dau-$version-windows-$arch.exe']);
+  }
+}
+
+class Linux implements BuildStrategy {
+  static bool get isX64 {
+    return Platform.version.contains("x64");
+  }
+
+  static bool get isARM64 {
+    return Platform.version.contains("arm64");
+  }
+
+  static bool get isRISCV {
+    return Platform.version.contains("riscv64");
+  }
+
+  @override
+  void build(String version, String arch) {
+    Directory('build').createSync(recursive: true);
+    Process.runSync('dart', ['compile','exe','bin/deltazx_authenticator.dart','-o','build/dau-$version-linux-$arch']);
+  }
+}
+
+class MacOS implements BuildStrategy {
+  static bool get isX64 {
+    return Platform.version.contains("x64");
+  }
+
+  static bool get isARM64 {
+    return Platform.version.contains("arm64");
+  }
+  
+  @override
+  void build(String version, String arch) {
+    Directory('build').createSync(recursive: true);
+    Process.runSync('dart', ['compile','exe','bin/deltazx_authenticator.dart','-o','build/dau-$version-macos-$arch']);
+  }
+
+}
+
+class Android implements BuildStrategy {
+  static bool get isARM64 {
+    return Platform.version.contains("arm64");
+  }
+
+  @override
+  void build(String version, String arch) {
+    Directory('build').createSync(recursive: true);
+    Process.runSync('dart', ['compile','exe','bin/deltazx_authenticator.dart','-o','build/dau-$version-android-$arch']);
   }
 }
